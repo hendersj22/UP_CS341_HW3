1bce27718bb4132f2b755a9af00f0f77e11705bb
Josh Henderson <hendersj22@up.edu>
2021-02-16 14:32:42 -0800

    fixed unit test issues

--- hw3/tests/readfile.test.js
+++ hw3/tests/readfile.test.js
@@ -7,7 +7,8 @@ test('test selectEvent', () => {
 	var html = fs.readFileSync('public/index.html', 'utf8');
 	expect(html).toEqual(expect.anything()); //any non-null value is okay
 
-//put the HTML into a testing DOM and do a sanity check document.body.innerHTML = html;
+//put the HTML into a testing DOM and do a sanity check 
+	document.body.innerHTML = html;
 	const $ = require('jquery');
 	expect($('h1').html()).toBe("Cheesecake Order Form");
 });
